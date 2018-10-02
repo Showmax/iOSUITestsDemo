9870460d878be168ba7069e6f68f126a1f85481b
Dominik Bucher <dominik.bucher@showmax.com>
2018-10-02 10:15:15 +0200

    Delete unused code

--- 📸/📸.swift
+++ 📸/📸.swift
@@ -68,15 +68,4 @@ private extension 📸 {
         ]
         return movies
     }
-
-    func wait(for duration: TimeInterval) {
-        let waitExpectation = expectation(description: "Waiting")
-
-        let when = DispatchTime.now() + duration
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            waitExpectation.fulfill()
-        }
-
-        waitForExpectations(timeout: duration+1)
-    }
 }
